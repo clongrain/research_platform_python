62160f3f96ef56f75d249a55f4529a8680924580
CLrain <2394617038@qq.com>
2024-09-11 13:11:28 +0800

    update .env and generate material function

--- material/views.py
+++ material/views.py
@@ -23,11 +23,10 @@ class SupabaseClientSingleton:
             SupabaseClientSingleton._instance = create_client(SUPABASE_URL, SUPABASE_KEY)
         return SupabaseClientSingleton._instance
 
+
 # Create your views here.
 def generateMaterial(request):
     data = json.loads(request.body)
-    order = data['order']
-
     writer = PdfWriter()
     pdfTemplate = select_pdf_template(data['materialType'])
     if not pdfTemplate:
@@ -37,10 +36,8 @@ def generateMaterial(request):
         writer.add_page(page)
 
     supabase = SupabaseClientSingleton.get_instance()
-    supabase.auth.sign_in_with_password(
-        {"email": "ChangyuYun@outlook.com", "password": "123456"})
-    for item in order:
-        mergePDF(writer, item, supabase, data)
+
+    mergePDF(writer, supabase, data)
     with open('merged_file.pdf', 'wb') as f:
         writer.write(f)
     with open('merged_file.pdf', 'rb') as f:
@@ -49,38 +46,19 @@ def generateMaterial(request):
         return response
 
 
-def mergePDF(writer, type: str, supabase, data):
-    table = ''
-    if type.__eq__('conferencePaper'):
-        table = 'conference_paper'
-    elif type.__eq__('journalPaper'):
-        table = 'journal_paper'
-    elif type.__eq__('award'):
-        table = 'award'
-    elif type.__eq__('patent'):
-        table = 'patent'
-    elif type.__eq__('monograph'):
-        table = 'monograph'
-    elif type.__eq__('softwareCopyright'):
-        table = 'software_copyright'
-    else:
-        return
+def mergePDF(writer, supabase, data):
+    for path in data['paths']:
+        res = supabase.storage.from_(BUCKET_NAME).download(path)
+        filename = path.split('/')[-1]
+        with open(filename, 'wb+') as f:
+            f.write(res)
+        print(f'文件：{filename}已下载')
+        reader = PdfReader(filename)
+        for page in reader.pages:
+            writer.add_page(page)
 
-    response = (supabase.table(table).select("storage_path").
-                in_('id', data[type]).execute())
-    if response.data and len(response.data) > 0:
-        for storage in response.data:
-            path: str = storage['storage_path']
-            res = supabase.storage.from_(BUCKET_NAME).download(path)
-            filename = path.split('/')[-1]
-            with open(filename, 'wb+') as f:
-                f.write(res)
-            print(f'文件：{filename}已下载')
-            reader = PdfReader(filename)
-            for page in reader.pages:
-                writer.add_page(page)
+        os.remove(filename)
 
-            os.remove(filename)
 
 def select_pdf_template(material_type):
     templates = {
